fce53170a8f75590d0bafce4d4f90fae3d7e6626
Benz <Benz@10.102.83.79>
2018-02-15 19:20:29 -0800

    Modified for generating the API properly in source folder

---
+++ src/main/java/com/benz/tutorial/jhipster/gateway/web/api/MoviesApiController.java
@@ -0,0 +1,27 @@
+package com.benz.tutorial.jhipster.gateway.web.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-02-15T16:15:07.332-08:00")
+
+@Controller
+public class MoviesApiController implements MoviesApi {
+
+	@Override
+	public ResponseEntity<Void> findMovie(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<Void> getAllMovies() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+}
